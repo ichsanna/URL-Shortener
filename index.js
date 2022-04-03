c1e5bd03ed61e360f02da7eaf7048559445aed83
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-04-03 17:22:30 +0700

    Test: add different .env for testing

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+
+if (path.basename(process.argv[1])==="jest.js") require('dotenv').config({path: `${__dirname}/.env.test`})
+else require('dotenv').config({path: `${__dirname}/.env.development`})
+console.log(process.env.MONGO_URL)
 
 const userRoutes = require('./routes/userRouter')
 const linkRoutes = require('./routes/linkRouter')
