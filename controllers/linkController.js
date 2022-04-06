a34a555d1fed3427483194ab6ce77da4d82fcbdf
Ichsan Nur Aldiansyah <43778566+ichsanna@users.noreply.github.com>
2022-04-06 13:12:13 +0700

    Revert "Bugfix"

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const randomString = require('randomstring');
 const mongoose = require('mongoose')
 
 const Link = require('../models/linkModel')
-const User = require('../models/userModel')
 const msg = require('../configs/responseMessages')
 const resFormat = require('../configs/responseFormat')
 
@@ -10,34 +10,20 @@ const linkMethods = {
     addNewLink: async (req, res) => {
         try {
             let userId = mongoose.Types.ObjectId(req.body.userId);
-            let { nameLink, longLink } = req.body
+            let nameLink = req.body.nameLink
+            let longLink = req.body.longLink
             let shortLink = randomString.generate({
                 length: 8,
                 charset: 'alphanumeric'
             })
-            let user = await User.findById(userId)
-            if (user) {
-                if (nameLink && longLink) {
-                    let newLink = new Link({
-                        owner: userId,
-                        nameLink: nameLink,
-                        longLink: longLink,
-                        shortLink: shortLink
-                    })
-                    let savedLink = await newLink.save()
-                    resStatus = 201
-                    resData = resFormat(true, msg.successCreateLink, savedLink)
-
-                }
-                else {
-                    resStatus = 422
-                    resData = resFormat(false, msg.failedCreateLink, null)
-                }
-            } else {
-                resStatus = 404
-                resData = resFormat(false, msg.noUserFoundById, null)
-            }
-            res.status(resStatus).json(resData)
+            let newLink = new Link({
+                owner: userId,
+                nameLink: nameLink,
+                longLink: longLink,
+                shortLink: shortLink
+            })
+            await newLink.save()
+            res.status(201).json(resFormat(true, msg.successCreateLink, ''))
         }
         catch (err) {
             res.status(400).json(resFormat(false, null, err))
@@ -45,74 +31,36 @@ const linkMethods = {
     },
     editLink: async (req, res) => {
         try {
-            let { linkId, nameLink, longLink } = req.body;
-            let resStatus, resData
-            if (linkId) {
-                let editedLink = await Link.findOneAndUpdate({ _id: linkId }, { $set: { nameLink: nameLink, longLink: longLink } })
-                if (editedLink) {
-                    resStatus = 200
-                    resData = resFormat(true, msg.successEditLink, editedLink)
-                }
-                else {
-                    resStatus = 404
-                    resData = resFormat(true, msg.noLinkFoundByLinkId, null)
-                }
-            }
-            else {
-                resStatus = 422
-                resData = resFormat(false, msg.failedEditLink, null)
-            }
-            res.status(resStatus).json(resData)
+            let linkId = req.body.linkId;
+            let nameLink = req.body.nameLink
+            let longLink = req.body.longLink
+            await Link.findOneAndUpdate({_id: linkId},{$set:{nameLink: nameLink,longLink: longLink}})
+            res.status(201).json(resFormat(true, msg.successEditLink, ''))
         }
         catch (err) {
             res.status(400).json(resFormat(false, null, err))
         }
     },
-    deleteLink: async (req, res) => {
-        try {
+    deleteLink: async (req,res) =>{
+        try{
             let linkId = req.body.linkId
-            let resStatus, resData
-            if (linkId) {
-                let deletedLink = await Link.findByIdAndDelete(linkId)
-                if (deletedLink) {
-                    resStatus = 200
-                    resData = resFormat(true, msg.successDeleteLink, null)
-                }
-                else {
-                    resStatus = 404
-                    resData = resFormat(true, msg.noLinkFoundByLinkId, null)
-                }
-            }
-            else {
-                resStatus = 422
-                resData = resFormat(true, msg.failedDeleteLink, null)
-            }
-            res.status(resStatus).json(resData)
+            await Link.findByIdAndDelete(linkId)
+            res.status(201).json(resFormat(true, msg.successDeleteLink, ''))
         }
-        catch (err) {
+        catch(err){
             res.status(400).json(resFormat(false, null, err))
         }
     },
     getLinkById: async (req, res) => {
         try {
-            let linkId = req.params.id
-            let resStatus, resData
-            if (linkId) {
-                let data = await Link.findById(linkId)
-                if (data) {
-                    resStatus = 200
-                    resData = resFormat(true, msg.successGetLink, data)
-                }
-                else {
-                    resStatus = 404
-                    resData = resFormat(false, msg.noLinkFoundByLinkId, null)
-                }
+            let id = req.params.id
+            let data = await Link.findById(id)
+            if (data) {
+                res.status(200).json(resFormat(true, msg.successGetLink, data))
             }
             else {
-                resStatus = 422
-                resData = resFormat(false, msg.failedGetLink, null)
+                res.status(404).json(resFormat(false, msg.noLinkFoundById, data))
             }
-            res.status(resStatus).json(resData)
         }
         catch (err) {
             res.status(400).json(resFormat(false, null, err))
@@ -121,23 +69,13 @@ const linkMethods = {
     getLinksByUser: async (req, res) => {
         try {
             let userId = req.params.id
-            let resStatus, resData
-            if (userId) {
-                let links = await Link.find({ owner: userId }).exec()
-                if (links) {
-                    resStatus = 200
-                    resData = resFormat(true, msg.successGetLinks, links)
-                }
-                else {
-                    resStatus = 404
-                    resData = resFormat(false, msg.noLinkFoundByUserId, null)
-                }
+            let links = await Link.find({ owner: userId }).exec()
+            if (links) {
+                res.status(200).json(resFormat(true, msg.successGetLinks, links))
             }
             else {
-                resStatus = 422
-                resData = resFormat(false, msg.failedGetLinks, null)
+                res.status(404).json(resFormat(false, msg.noLinkFound, data))
             }
-            res.status(resStatus).json(resData)
         }
         catch (err) {
             res.status(400).json(resFormat(false, null, err))
