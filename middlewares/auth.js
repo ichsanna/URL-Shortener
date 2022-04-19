f50dc3693a9f1cc0ee469c83a06f262da110ea35
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-04-19 15:33:42 +0700

    Feat: Add username to navbar, resolves #19

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,28 +4,28 @@ const resFormat = require('../configs/responseFormat')
 const msg = require('../configs/responseMessages')
 
 const auth = {
-    verifyToken: async (req,res,next)=>{
+    verifyToken: async (req, res, next) => {
         try {
             const token = req.headers.authorization.split(' ')[1]
-            let decoded =  auth.decodeToken(token)
+            let decoded = auth.decodeToken(token)
             let username = decoded.username
-            let user = await User.findOne({username: username})
+            let user = await User.findOne({ username: username })
             if (user) next()
         } catch (err) {
-            return res.status(401).json(resFormat(false,msg.failedAuth,err))
+            return res.status(401).json(resFormat(false, msg.failedAuth, err))
         }
     },
-    decodeToken: (token)=>{
+    decodeToken: (token) => {
         return jwt.verify(token, process.env.JWT_SECRET)
     },
-    verifyTokenWeb: async (token)=>{
+    verifyTokenWeb: async (token) => {
         try {
             if (!token) return false
-            let decoded =  auth.decodeToken(token)
+            let decoded = auth.decodeToken(token)
             let username = decoded.username
-            let user = await User.findOne({username: username})
-            if (user) return true
-            else return false
+            let user = await User.findOne({ username: username })
+            if (user) return {isAuthorized: true, username: username}
+            else return {isAuthorized: false, username: null}
         } catch (err) {
             return false
         }
