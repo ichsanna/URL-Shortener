bae0e52a420dd2536e45e59241cedae9ed7ec294
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-04-19 15:49:00 +0700

    Fix: Fixes #20

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,14 +4,13 @@ const logout = () => {
     Cookies.set("user_id", "")
     location.reload()
 }
-const renderLinks = async (items, sortby = 'name', order = 'asc') => {
+const renderLinks = async (items) => {
     $("#add-name").val("")
     $("#add-long").val("")
     $('#edit-name').val("")
     $('#edit-long').val("")
     $('#edit-short').val("")
     $(".links-container").remove()
-    items = sortLinks(items, sortby, order)
     items.forEach((item, count) => {
         created_at = ((new Date(item.created_at)).toString()).substring(4, 21)
         $('.main-container').append(`<div class="container links-container d-flex justify-content-between">
@@ -130,59 +129,59 @@ const deleteLink = async (linkId) => {
     links.splice(index, 1)
     renderLinks(links)
 }
-const searchLink = () => {
+const filterLinks = () => {
+    let search = $('.search-input').val()
+    let sortBy = $('.sort-by').val()
+    let sortOrder = $('.sort-order').val()
     tmpLinks = links
-    tmpLinks = tmpLinks.filter(arr => arr.nameLink.toLowerCase().includes($('.search-input').val()))
-    renderLinks(tmpLinks)
-}
-const sortLinks = (items, sortby, order) => {
-    if (sortby === 'name') {
-        items.sort((a, b) => {
+    //search dulu baru ini rendernya links, bukan tmpLinks
+    if (sortBy === 'name') {
+        tmpLinks.sort((a, b) => {
             if (a.nameLink.toLowerCase() > b.nameLink.toLowerCase()) {
                 return 1
             }
             return -1
         })
     }
-    else if (sortby === 'short') {
-        items.sort((a, b) => {
+    else if (sortBy === 'short') {
+        tmpLinks.sort((a, b) => {
             if (a.shortLink.toLowerCase() > b.shortLink.toLowerCase()) {
                 return 1
             }
             return -1
         })
     }
-    else if (sortby === 'date') {
-        items.sort((a, b) => {
+    else if (sortBy === 'date') {
+        tmpLinks.sort((a, b) => {
             if (a.created_at.toLowerCase() > b.created_at.toLowerCase()) {
                 return 1
             }
             return -1
         })
     }
-    else if (sortby === 'long') {
-        items.sort((a, b) => {
+    else if (sortBy === 'long') {
+        tmpLinks.sort((a, b) => {
             if (a.longLink.toLowerCase() > b.longLink.toLowerCase()) {
                 return 1
             }
             return -1
         })
     }
-    if (order == 'desc') {
-        items.reverse()
+    if (sortOrder == 'desc') {
+        tmpLinks.reverse()
     }
-    console.log(items)
-    return items
+    tmpLinks = tmpLinks.filter(arr => arr.nameLink.toLowerCase().includes(search))
+    renderLinks(tmpLinks)
 }
 $(document).ready(() => {
     getLinks()
     $('.search-input').on({
-        keyup: $.debounce(500, searchLink)
+        keyup: $.debounce(500, filterLinks)
     });
     $('.sort-by').on('change', function () {
-        renderLinks(links,$('.sort-by').val(),$('.sort-order').val())
+        filterLinks()
     });
     $('.sort-order').on('change', function () {
-        renderLinks(links,$('.sort-by').val(),$('.sort-order').val())
+        filterLinks()
     });
 })
