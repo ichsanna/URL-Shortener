c03aecc3e837e97aab3fc8d277f8d09e1c74e271
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-04-25 11:42:31 +0700

    Fix: console log server start

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const webRoutes = require('./routes/webRouter')
 const resFormat = require('./configs/responseFormat')
 const msg = require('./configs/responseMessages')
 
+const port = process.env.PORT || 3000
 const app = express()
 
 app.set('view engine','ejs')
@@ -41,7 +42,7 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500).json(resFormat(false,msg.invalidRoute,null));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server started on port 3000')
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
 });
 module.exports = app
